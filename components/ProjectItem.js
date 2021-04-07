8eec16ef5304d23692c8a6cc4680dc11d647679c
Daniel Todd <aaron.mercado@finxp.com>
2021-04-07 11:00:07 +0800

    WIP: Move tailwind class to sass - WIP: Responsiveness

--- components/ProjectItem.js
+++ components/ProjectItem.js
@@ -3,8 +3,8 @@ import Link from 'next/link';
 export default function ProjectItem({ project }) {
   return (
     <Link href={project.url} passHref={true}>
-      <a className="px-2 mb-4 flex flex-col w-2/3 my-2" rel="noreferrer" target="_blank">
-        <div className="w-full bg-white rounded-md shadow-sm border p-6 hover:shadow-lg transition duration-300 ease-in-out">
+      <a className="project--item" rel="noreferrer" target="_blank">
+        <div className="project--card">
           <h3 className={(!project.isActive ? 'line-through' : '') + ' text-xl font-bold'}>
             {project.title}
           </h3>
@@ -13,7 +13,7 @@ export default function ProjectItem({ project }) {
             {project.tags.map((tag, index) => (
               <div
                 key={index}
-                className="rounded-full bg-blue-300 text-sm text-white py-2 px-3 font-bold mx-1">
+                className="tag--item tag--rounded bg-blue-300">
                 {tag}
               </div>
             ))}
