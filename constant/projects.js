33bc41223a61fb4e3fa40be584b9413022eb5094
ronmrcdo <ronmercadoaa@gmail.com>
2021-04-10 21:33:09 +0800

    chore: Added new Project Portfolio

--- constant/projects.js
+++ constant/projects.js
@@ -25,5 +25,13 @@ export const projects = [
     'Starter landing page built using Nuxt and Tailwindcss',
     'https://github.com/ronmrcdo/nuxt-tailwind-landing',
     ['Nuxt', 'Vue', 'Tailwindcss', 'Landing Page']
+  ),
+
+  new Project(
+    3,
+    'My Portfolio',
+    'I made my portfolio an open sourced built using Next.js, Tailwindcss, Sass, and etc.',
+    'https://github.com/ronmrcdo/next-portfolio',
+    ['Next.js', 'Tailwindcss', 'Portfolio', 'SCSS']
   )
 ];
